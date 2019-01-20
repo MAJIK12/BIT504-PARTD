e0458e29cfa57aeba122068fcf0bc42584c527e4
MAJIK12 <orphansource@gmail.com>
2019-01-20 17:00:45 +1300

    V0.04

--- BIT504_PARTD/src/Main.java
+++ BIT504_PARTD/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 		System.out.println("HI, my name is nic");
 		System.out.println("My favorite colour is blue");
 		System.out.println("My favorite mode of transport is rolling");
+		System.out.println("The best day of the week is Sunday");
 	}
 
 }
